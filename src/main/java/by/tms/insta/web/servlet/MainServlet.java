f1491eb8b8d8d22557e51f870e91c8b16c236aab
simonpirko <sipi7gm@gmail.com>
2023-04-17 19:40:43 +0300

    Fix

--- src/main/java/by/tms/insta/web/servlet/MainServlet.java
+++ src/main/java/by/tms/insta/web/servlet/MainServlet.java
@@ -17,5 +17,6 @@ public class MainServlet extends HttpServlet {
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
     getServletContext().getRequestDispatcher("/pages/main.jsp").forward(req, resp);
     System.out.println("Hello World");
+    //fix
   }
 }
