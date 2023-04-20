26668d8d9403d44b2dad1853fbfbbd7e3e1f39d7
Yuri-Vanderon <garanovich2@gmail.com>
2023-04-20 14:40:38 +0300

    added UserDto

---
+++ src/main/java/by/tms/insta/web/Dto/UserDto.java
@@ -0,0 +1,23 @@
+package by.tms.insta.web.Dto;
+
+public class UserDto {
+    private String username;
+    private String photo;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+}
+
