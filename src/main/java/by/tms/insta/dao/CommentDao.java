43ac872ca4593f6746096c40de0eadb2b3d7b468
VDN_dude <garanovich2@gmail.com>
2023-04-25 20:29:33 +0300

    added method findByPostId (class in draft)

--- src/main/java/by/tms/insta/dao/CommentDao.java
+++ src/main/java/by/tms/insta/dao/CommentDao.java
@@ -1,9 +1,12 @@
 package by.tms.insta.dao;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import javax.xml.stream.events.Comment;
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 public class CommentDao {
     private static CommentDao instance;
@@ -11,6 +14,7 @@ public class CommentDao {
     private static final String WRITE_COMMENT = "insert into comments (message, post_id, user_id, created_at) value(?,?,?,?)";
     private static final String DELETE_COMMENT = "delete from comments where comment_id = ?";
     private static final String UPDATE_COMMENT = "update comments set message = ? where comment_id = ?";
+    private static final String SELECT_BY_POST_ID = "select * from comments where post_id = ?";
     private CommentDao() {
     }
     public CommentDao getInstance(){
@@ -46,6 +50,26 @@ public class CommentDao {
             preparedStatement.setString(1, message);
             preparedStatement.setInt(2, comment_id);
             preparedStatement.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public List<Comment> findByPostId(int post_id){
+        try {
+            List<Comment> commentList = new ArrayList<Comment>();
+            PreparedStatement preparedStatement = connection.prepareStatement(SELECT_BY_POST_ID);
+            preparedStatement.setInt(1, post_id);
+            ResultSet resultSet = preparedStatement.getResultSet();
+            while (resultSet.next()){
+                int comment_id = resultSet.getInt(1);
+                String string = resultSet.getString(2);
+                int user_id = resultSet.getInt(4);
+                LocalDateTime time = resultSet.getTimestamp(5).toLocalDateTime();
+                Comment comment = new Comment();
+                commentList.add(comment);
+            }
+            Collections.sort(commentList);
+            return commentList;
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
