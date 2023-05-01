217348301a46ae89fcfb1b6db3856c6230dfb963
Denis Smirnov <den170193@yandex.ru>
2023-05-01 14:46:33 +0300

    added login and logout servlets

---
+++ src/main/java/by/tms/insta/web/servlet/LogoutServlet.java
@@ -0,0 +1,20 @@
+package by.tms.insta.web.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author Denis Smirnov on 01.05.2023
+ */
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getSession().invalidate();
+        resp.sendRedirect("/");
+    }
+}
