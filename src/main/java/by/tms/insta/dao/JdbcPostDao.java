0ba76afe5d6872283876495bb825f5cae53d0a7a
artemKrivko <krivko219319@gmail.com>
2023-05-03 18:10:39 +0300

    update createPost

--- src/main/java/by/tms/insta/dao/JdbcPostDao.java
+++ src/main/java/by/tms/insta/dao/JdbcPostDao.java
@@ -21,10 +21,11 @@ public class JdbcPostDao implements PostDao {
     public void createPost(Post post) throws IOException {
 
         try {
-            PreparedStatement statement = connection.prepareStatement("INSERT INTO posts (user_id, image, createdAt) VALUES (?,?,?)");
-            statement.setString(1, String.valueOf(post.getAuthor().getId())); // почему сетаем стрингу а не инт и просто передаем айди
+            PreparedStatement statement = connection.prepareStatement("INSERT INTO posts ( user_id, image, created_at, description) VALUES (?,?,?,?)");
+            statement.setInt(1, (int) post.getAuthor().getId());
             statement.setString(2, post.getImage());
             statement.setTimestamp(3, Timestamp.valueOf(post.getCreatedAt()));
+            statement.setString(4, post.getDescription());
             statement.executeUpdate();
 
         } catch (Exception e) {
@@ -62,8 +63,8 @@ public class JdbcPostDao implements PostDao {
 
         try {
 
-            PreparedStatement statement = connection.prepareStatement("UPDATE post SET (user_id, image, createdAt) VALUES (?,?,?) WHERE post_id =?  ");
-            statement.setLong(1, postId);
+            PreparedStatement statement = connection.prepareStatement("UPDATE post SET (post_id, user_id, image, created_at, description) VALUES (?,?,?,?,?) WHERE post_id =?  ");
+            statement.setInt(1, postId);
             statement.setString(2, post.getImage());
             statement.setTimestamp(3, Timestamp.valueOf(post.getCreatedAt()));
             statement.setLong(4, postId);
@@ -95,9 +96,9 @@ public class JdbcPostDao implements PostDao {
             ResultSet resultSet = statement.executeQuery();
             List<Post> posts = new ArrayList<>();
             while (resultSet.next()) {
-                post = Post.builder().setId(resultSet.getInt("id")).setAuthor(user)
+                post = Post.builder().setId(resultSet.getInt("post_id")).setAuthor(user)
                         .setImage(resultSet.getString("image"))
-                        .setCreatedAt(Timestamp.valueOf(resultSet.getString("createdAt")).toLocalDateTime()).build();
+                        .setCreatedAt(Timestamp.valueOf(resultSet.getString("created_at")).toLocalDateTime()).build();
 
                 posts.add(post);
 
@@ -122,11 +123,11 @@ public class JdbcPostDao implements PostDao {
             while (resultSet.next()) {
                 Long userId = resultSet.getLong("post_id");
                 Comment comment = Comment.builder()
-                        .setId(resultSet.getInt("id"))
+                        .setId(resultSet.getInt("comment_id"))
                         .setAuthor(User.builder().build())// todo before add UserDAO)
                         .setPost(post)
                         .setMessage(resultSet.getString("message"))
-                        .setCreateAt(Timestamp.valueOf(resultSet.getString("createdAt")).toLocalDateTime()).build();
+                        .setCreateAt(Timestamp.valueOf(resultSet.getString("created_at")).toLocalDateTime()).build();
 
 
                 comments.add(comment);
