95dc1a16a891701d09856a0be6cf657d21857cb1
VDN_dude <garanovich2@gmail.com>
2023-05-04 15:29:43 +0300

    deleted method toEntity

--- src/main/java/by/tms/insta/mapper/UserMapper.java
+++ src/main/java/by/tms/insta/mapper/UserMapper.java
@@ -3,20 +3,17 @@ package by.tms.insta.mapper;
 import by.tms.insta.dto.UserDto;
 import by.tms.insta.entity.User;
 
-import java.util.Optional;
 
 public class UserMapper {
-    private final UserDao userDao = JDBCUserDao.getInstance();
+    private UserMapper() {
+    }
 
-    public UserDto toDto (User user){
+    public static UserDto toDto (User user){
         UserDto userDto = new UserDto();
         userDto.setName(user.getName());
         userDto.setUsername(user.getUsername());
         userDto.setEmail(user.getEmail());
         userDto.setAvatar(user.getAvatar());
         return userDto;
-    }
-    public User toEntity(UserDto userDto){
-        return userDao.findByUsername(userDto.getUsername()).get();
     }
 }
