61e3a396819207060647d5862c46e32a28223a86
SalomeCarter <elizabethboston@icloud.com>
2023-05-08 13:46:48 +0400

    Security Filter

---
+++ src/main/java/by/tms/insta/web/servlet/SecurityFilter.java
@@ -0,0 +1,25 @@
+package by.tms.insta.web.servlet;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter("/user/*")
+public class SecurityFilter extends HttpFilter {
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+
+        getServletContext().getRequestDispatcher("/pages/filter.jsp").forward(req, res);
+
+        if (req.getSession().getAttribute("user") == null) {
+            res.sendError(HttpServletResponse.SC_BAD_REQUEST);
+        } else {
+            chain.doFilter(req, res);
+        }
+
+    }
+}
