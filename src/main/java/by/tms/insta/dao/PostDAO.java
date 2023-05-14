67bd29d8c91415bf9377a43385b69371af6cffed
Yuri-Vanderon <garanovich2@gmail.com>
2023-05-14 13:43:54 +0300

    added SQLException to each method, sql constants, added methods like, unlike, extractCountOfLikes in JDBCPostDAO, deletedByPostId int JDBCCommentDAO

--- src/main/java/by/tms/insta/dao/PostDAO.java
+++ src/main/java/by/tms/insta/dao/PostDAO.java
@@ -1,25 +1,25 @@
 package by.tms.insta.dao;
 
-import by.tms.insta.entity.Comment;
 import by.tms.insta.entity.Post;
-import by.tms.insta.entity.User;
 
-import java.io.IOException;
 import java.sql.SQLException;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 public interface PostDAO {
 
-    void createPost(Post post);
+    void save(Post post) throws SQLException;
 
-    Post findPostById (int id);
+    Optional<Post> findPostById (int id) throws SQLException;
 
-    void updatePost(Post post, int postId);
+    void updatePost(int postId, String image, String description, LocalDateTime createdAt) throws SQLException;
 
-    void deletePost(int id);
+    void deletePost(int id) throws SQLException;
 
-    List<Post> getPostsByUser(User user);
-
-    List<Comment> getCommentsByPost(Post post);
+    List<Post> getPostsByUserId(int userId) throws SQLException;
+    void Like(int user_id, int post_id) throws SQLException;
+    void unLike(int user_id) throws SQLException;
+    int extractCountOfLikes(int post_id) throws SQLException;
 
 }
