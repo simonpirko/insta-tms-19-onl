c9eaeaf03cc64d3cd5da82d79a9d273971b2f7b5
Yuri-Vanderon <garanovich2@gmail.com>
2023-05-16 12:08:44 +0300

    changed url

--- src/main/java/by/tms/insta/web/servlet/AddCommentServlet.java
+++ src/main/java/by/tms/insta/web/servlet/AddCommentServlet.java
@@ -14,7 +14,7 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.time.LocalDateTime;
 
-@WebServlet("user/createcomment")
+@WebServlet("user/addcomment")
 public class AddCommentServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
