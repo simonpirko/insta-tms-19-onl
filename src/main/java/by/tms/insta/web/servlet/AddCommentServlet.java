fe7bea5951dbd7d8beb4a775be956ff7345e1a17
Yuri-Vanderon <garanovich2@gmail.com>
2023-05-17 12:05:07 +0300

    changed values

--- src/main/java/by/tms/insta/web/servlet/AddCommentServlet.java
+++ src/main/java/by/tms/insta/web/servlet/AddCommentServlet.java
@@ -1,9 +1,12 @@
 package by.tms.insta.web.servlet;
 
+import by.tms.insta.dto.UserDto;
 import by.tms.insta.entity.Comment;
 import by.tms.insta.entity.Post;
 import by.tms.insta.entity.User;
 import by.tms.insta.service.CommentService;
+import by.tms.insta.service.PostService;
+import by.tms.insta.service.UserService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -13,28 +16,46 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @WebServlet("user/addcomment")
 public class AddCommentServlet extends HttpServlet {
+    private final PostService postService = PostService.getInstance();
+    private final UserService userService = UserService.getInstance();
+    private final CommentService commentService = CommentService.getInstance();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String commentMessage = req.getParameter("commentMessage");
-        Object[] commentBody = req.getParameter("commentBody").split("&");
-        User author = (User) commentBody[0];
-        Post post = (Post) commentBody[1];
-        LocalDateTime now = LocalDateTime.now();
-        Comment build = Comment.builder()
-                .setAuthor(author)
-                .setPost(post)
-                .setMessage(commentMessage)
-                .setCreateAt(now)
-                .build();
-        CommentService instance = CommentService.getInstance();
+
+        UserDto sessionUser = (UserDto) req.getSession().getAttribute("user");
+
         try {
-            instance.save(build);
+            Optional<User> user = userService.findByUsername(sessionUser.getUsername());
+
+            int postId = Integer.parseInt(req.getParameter("postId"));
+            Optional<Post> post = postService.findPostById(postId);
+
+            if (user.isPresent() & post.isPresent()) {
+                String commentMessage = req.getParameter("commentMessage");
+                LocalDateTime now = LocalDateTime.now();
+                Comment build = Comment.builder()
+                        .setAuthor(user.get())
+                        .setPost(post.get())
+                        .setMessage(commentMessage)
+                        .setCreateAt(now)
+                        .build();
+                commentService.save(build);
+
+                resp.sendRedirect(req.getContextPath() + "/viewpost?id=" + post.get().getId());
+
+            } else {
+                req.setAttribute("errormessage", "We can't found post.");
+                getServletContext().getRequestDispatcher("/pages/error.jsp").forward(req, resp);
+            }
+
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            req.setAttribute("errormessage", "Something went wrong on our side.");
+            getServletContext().getRequestDispatcher("/pages/error.jsp").forward(req, resp);
         }
-        resp.sendRedirect(req.getContextPath() + "/viewpost?id=" + post.getId());
     }
 }
