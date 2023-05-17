b014dce14542f8888869948f27d8edfe4022afe4
Denis Smirnov <den170193@yandex.ru>
2023-05-17 11:05:56 +0300

    added DeleteUserServlet

---
+++ src/main/java/by/tms/insta/web/servlet/DeleteUserServlet.java
@@ -0,0 +1,37 @@
+package by.tms.insta.web.servlet;
+
+import by.tms.insta.entity.User;
+import by.tms.insta.service.UserService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+
+/**
+ * @author Denis Smirnov on 12.05.2023
+ */
+@WebServlet("/user/account")
+public class DeleteUserServlet extends HttpServlet {
+    private final String USER = "user";
+    private final UserService userService = UserService.getInstance();
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        User user = (User) req.getSession().getAttribute(USER);
+
+        try {
+
+            userService.deleteById(user.getId());
+            resp.sendRedirect("/register");
+
+        } catch (SQLException e) {
+            req.setAttribute("errormessage", "Something went wrong on our side.");
+            getServletContext().getRequestDispatcher("/pages/error.jsp").forward(req, resp);
+        }
+    }
+}
