924dafb0b05cfc9c2b4253def98babbc7c06948e
Yuri-Vanderon <garanovich2@gmail.com>
2023-05-17 12:10:49 +0300

    added redirect to error page

--- src/main/java/by/tms/insta/web/servlet/HomeServlet.java
+++ src/main/java/by/tms/insta/web/servlet/HomeServlet.java
@@ -1,5 +1,6 @@
 package by.tms.insta.web.servlet;
 
+import by.tms.insta.dto.UserDto;
 import by.tms.insta.entity.Post;
 import by.tms.insta.entity.User;
 import by.tms.insta.service.PostService;
@@ -13,6 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Optional;
 
 @WebServlet("/user/home")
 public class HomeServlet extends HttpServlet {
@@ -23,32 +25,43 @@ public class HomeServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        User sessionUser = (User) req.getAttribute("user");
+
+        UserDto userDto = (UserDto) req.getSession().getAttribute("user");
+
         try {
-            List<User> users = userService.extractFollowed(sessionUser.getId());
-            int postsPerPage = 5;
 
-            for (User user : users) {
-                int countOfPagesWithPosts = postService.getCountOfPagesWithPosts(user, postsPerPage);
-                countOfPages = countOfPages + countOfPagesWithPosts;
+            Optional<User> sessionUser = userService.findByUsername(userDto.getUsername());
+            if (sessionUser.isPresent()) {
+
+                List<User> users = userService.extractFollowed(sessionUser.get().getId());
+                int postsPerPage = 5;
+
+                for (User user : users) {
+                    int countOfPagesWithPosts = postService.getCountOfPagesWithPosts(user, postsPerPage);
+                    countOfPages = countOfPages + countOfPagesWithPosts;
+                }
+
+                req.setAttribute("countOfPages", countOfPages);
+
+                String page = req.getParameter("page");
+                if (page == null) {
+                    req.setAttribute("page", 1);
+                }
+
+                int paginationOffset = Integer.parseInt(page) * postsPerPage;
+
+                List<Post> postList = postService.getFollowedUsersPosts(sessionUser.get().getId(), postsPerPage, paginationOffset);
+                req.setAttribute("postList", postList);
+
+                req.getRequestDispatcher("/pages/home.jsp").forward(req, resp);
+
             }
-
-            req.setAttribute("countOfPages", countOfPages);
-
-            String page = req.getParameter("page");
-            if (page == null){
-                req.setAttribute("page", 1);
-            }
-
-            int paginationOffset = Integer.parseInt(page) * postsPerPage;
-
-            List<Post> postList = postService.getFollowedUsersPosts(sessionUser.getId(), postsPerPage, paginationOffset);
-            req.setAttribute("postList", postList);
-
-            req.getRequestDispatcher("/pages/home.jsp").forward(req, resp);
+            req.setAttribute("errormessage", "Something went wrong.");
+            getServletContext().getRequestDispatcher("/pages/error.jsp").forward(req, resp);
 
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            req.setAttribute("errormessage", "Something went wrong on our side.");
+            getServletContext().getRequestDispatcher("/pages/error.jsp").forward(req, resp);
         }
 
     }
