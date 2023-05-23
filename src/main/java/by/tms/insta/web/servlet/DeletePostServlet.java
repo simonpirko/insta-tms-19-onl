766f2625b3fb30cbf2eb5a20290a36bb756a7142
SalomeCarter <elizabethboston@icloud.com>
2023-05-23 14:12:50 +0400

    Back Delete Post

--- src/main/java/by/tms/insta/web/servlet/DeletePostServlet.java
+++ src/main/java/by/tms/insta/web/servlet/DeletePostServlet.java
@@ -1,12 +1,15 @@
 package by.tms.insta.web.servlet;
 
+import by.tms.insta.entity.User;
 import by.tms.insta.service.PostService;
+import by.tms.insta.entity.SessionPrincipalUser;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.sql.SQLException;
 
@@ -15,12 +18,14 @@ public class DeletePostServlet extends HttpServlet {
     private final PostService postService = PostService.getInstance();
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        int id = Integer.parseInt(req.getParameter("id"));
-        int userId = Integer.parseInt(req.getParameter("userId"));
+        SessionPrincipalUser sessionUser = (SessionPrincipalUser) req.getSession().getAttribute("user");
+        int id = Integer.parseInt(req.getParameter("username"));
+        String username = sessionUser.getUsername();
+
 
         try {
             postService.deletePost(id);
-            resp.sendRedirect(req.getContextPath() + "/user/viewprofile?id=" + userId);
+            resp.sendRedirect(req.getContextPath() + "/user/account?id=" + username);
 
         } catch (SQLException e) {
             req.setAttribute("errormessage", "Something went wrong on our side");
